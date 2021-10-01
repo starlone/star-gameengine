3dcff0571b7df452ff0973f84527b45809960127
Starlone <starlone.passos@gmail.com>
2021-10-01 15:18:53 -0300

    refactor factory.rect

--- src/factory.ts
+++ src/factory.ts
@@ -1,38 +1,33 @@
 import { GameObject } from './gameobject';
 import { IFactoryOptions } from './options/factory.options';
-import { Point } from './point';
-import { MeshRenderer } from './renderers/mesh.renderer';
 
 export class Factory {
   static rect(opt: IFactoryOptions) {
-    const name = opt.name || '';
-
     var x = opt.x || 0;
     var y = opt.y || 0;
     var w = opt.w || 10;
     var h = opt.h || 10;
 
     const isStatic = opt.static !== undefined ? opt.static : false;
-    const hasRigidBody =
-      opt.hasRigidBody !== undefined ? opt.hasRigidBody : true;
+    const rigidBody = opt.hasRigidBody !== undefined ? opt.hasRigidBody : true;
 
-    const rend = new MeshRenderer({
-      color: opt.color || 'blue',
-    });
+    var vertices = [
+      { x: -w / 2, y: -h / 2 },
+      { x: w / 2, y: -h / 2 },
+      { x: -w / 2, y: h / 2 },
+      { x: w / 2, y: h / 2 },
+    ];
 
-    const obj = new GameObject({
-      name: name,
+    return new GameObject({
+      name: opt.name || '',
       position: { x, y },
-      renderer: rend,
       static: isStatic,
-      hasRigidBody: hasRigidBody,
+      hasRigidBody: rigidBody,
+      renderer: {
+        type: 'MeshRenderer',
+        color: opt.color || 'blue',
+      },
+      vertices: vertices,
     });
-
-    obj.vertices.push(new Point(-w / 2, -h / 2));
-    obj.vertices.push(new Point(w / 2, -h / 2));
-    obj.vertices.push(new Point(w / 2, h / 2));
-    obj.vertices.push(new Point(-w / 2, h / 2));
-
-    return obj;
   }
 }
