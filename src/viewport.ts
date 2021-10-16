2d2896dc5f5fbba355efb46dda25148790d7350d
Starlone <starlone.passos@gmail.com>
2021-10-15 23:17:55 -0300

    refactor: viewport

--- src/viewport.ts
+++ src/viewport.ts
@@ -64,8 +64,8 @@ export class ViewPort {
     return new Extent(
       this.pivot.x,
       this.pivot.y,
-      this.element.width,
-      this.element.height
+      this.getWidth(),
+      this.getHeight()
     );
   }
 
@@ -74,8 +74,8 @@ export class ViewPort {
     this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.ctx.scale(this.scale, this.scale);
 
-    var x = position.x - this.getWidth() / 2;
-    var y = position.y - this.getHeight() / 2;
+    var x = position.x - (this.getWidth() / 2);
+    var y = position.y - (this.getHeight() / 2);
     this.pivot.change(x, y);
     this.ctx.translate(-x, -y);
   }
