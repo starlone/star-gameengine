1dbb253556cf8f5b70f01cc8ffc0113b071bb586
Starlone <starlone.passos@gmail.com>
2021-12-03 16:13:10 -0300

    fix: prettier

--- src/scene.ts
+++ src/scene.ts
@@ -25,7 +25,7 @@ export class Scene {
 
     if (options.indexCamera === undefined) {
       this.camera = new GameObject({
-        name: 'MainCamera'
+        name: 'MainCamera',
         static: true,
         position: {
           x: 0,
